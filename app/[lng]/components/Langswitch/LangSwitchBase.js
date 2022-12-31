d880f19060663fa1ab7175d222fa673c35771feb
najat <najatt.ismail@gmail.com>
2022-12-31 14:06:47 +0200

    designed the langswitch

--- app/[lng]/components/Langswitch/LangSwitchBase.js
+++ app/[lng]/components/Langswitch/LangSwitchBase.js
@@ -4,17 +4,15 @@ import { languages } from '../../../i18n/settings'
 
 export const LangSwitchBase = ({ t, lng, path = '' }) => {
   return (
-<div>
-  <Trans i18nKey="languageSwitcher" t={t}>
-    Switch from <strong>{{lng}}</strong> to:{' '}
-  </Trans>
+<div class="absolute top-0 z-10 flex items-center justify-cemter">
   {languages.filter((l) => lng !== l).map((l, index) => {
     return (
       <span key={l}>
-        {index > 0 && (' or ')}
         <Link href={`/${l}${path}`} className="block bg-black text-white hover:bg-gray-700 font-bold py-2 px-4">
-          {l}
-        </Link>
+        <Trans i18nKey="languageSwitcher">
+          <strong>{l}</strong>
+        </Trans>
+       </Link>
       </span>
     )
   })}
